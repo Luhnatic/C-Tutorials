b7adcfafed81dc036b1e9a8f8c5e3911e886a113
Luhnatic <60678760+Luhnatic@users.noreply.github.com>
2024-10-28 11:23:50 -0700

    Initial Commit

---
+++ C++ Tutorials/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  C++ Tutorials
+//
+//  Created by Miles on 10/28/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
